2b8b91ab987d9d8a3fd8a4bdf36646b372953ddb
Aliaksei Slepau <aslepaugo@gmail.com>
2021-02-17 17:28:37 +0100

    fixed sanitizing and added response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
@@ -25,7 +25,7 @@ products = [
 ]
 
 def valid_product(product_object):
-    if "name" in product_object and "id" in product_object and "price" in product_object:
+    if ("name" in product_object and "id" in product_object and "price" in product_object):
         return True
     else:
         return False
@@ -38,10 +38,16 @@ def get_products():
 
 @app.route("/products", methods=['POST'])
 def add_book():
-    request_data = request.get_data()
+    request_data = request.get_json()
+    new_product = {
+        "id": request_data["id"],
+        "name": request_data["name"],
+        "price": request_data["price"]
+    }
     if valid_product(request_data):
-        products.insert(0, request_data)
-        return "True"
+        products.insert(0, new_product)
+        response = Response("", 201, mimetype='application/json')
+        return response
     else:
         return "False"
 
